006373953f63118ac2e27ddef09719f115881388
payalpatra <payalpatra105@gmail.com>
2022-01-15 21:09:50 +0530

    Day11: Given an n x m array where all rows and columns are in sorted order, write a function to determine whether the array contains an element x.

--- 11_MatrixSearch.js
+++ 11_MatrixSearch.js
@@ -1,4 +1,4 @@
-// Given an n x m array where all rows and columns are in sorted order, write a function to determine whether the array contains an element x.
+// 11. Given an n x m array where all rows and columns are in sorted order, write a function to determine whether the array contains an element x.
 
 /*
 
